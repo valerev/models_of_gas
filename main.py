a5d530e71df15feea6b27adbcbf5fe7bfc7bb4e2
valerev <valerev.da@phystech.edu>
2020-10-20 15:11:54 +0300

    Added some notunderstandable copypaste from Rafael's code, but it doesn't work anyway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 N = 20  # Кол-во делений пространства
 Per = 100  # Кол-ва делений времени
@@ -23,21 +24,33 @@ X, Y = np.meshgrid(space_x, space_y)
 concentration = np.zeros(N * N ).reshape( N, N)
 
 
-def fill_space():
-    for x in range(N):
-        for y in range(N):
-            for v_x in range(number_of_velocities):
-                for v_y in range(number_of_velocities):
-                    f[0][x][y][v_x][v_y] = np.exp(-((dx * x - a)**2 +
-                                          (dy * y - a)**2)/ 2)  # Начальные условия
+def fill_space(is_one_velocity = False):
+    if is_one_velocity:
+        for x in range(N):
+            for y in range(N):
+                        f[0][x][y][2][2] = np.exp(-((dx * x - a)**2 +
+                                              (dy * y - a)**2)/ 2)  # Начальные условия
+    else:
+        for x in range(N):
+            for y in range(N):
+                for v_x in range(number_of_velocities):
+                    for v_y in range(number_of_velocities):
+                        f[0][x][y][v_x][v_y] = np.exp(-((dx * x - a)**2 +
+                                              (dy * y - a)**2)/ 2)  # Начальные условия
 
 
-def draw_3d(c, n):
-    fig = plt.figure()  # creating an image
-    ax = plt.axes(xlim=(2 * dx, dx * (N - 2)), ylim=(2 * dy, dy * (N - 2)), zlim=(0, 100), projection='3d')
-    ax.plot_surface(X, Y, c, cmap='Blues')
-    fig.savefig('gas' + '{0:d}'.format(n + 1000) + '.jpg')
-    plt.close('all')
+fig = plt.figure()  # creating an image
+ax = plt.axes(xlim=(2 * dx, dx * (N - 2)), ylim=(2 * dy, dy * (N - 2)), zlim=(0, 100), projection='3d')
+
+
+def draw_3d(c):
+    ax.clear()
+    return ax.plot_surface(X, Y, c, cmap='Blues')
+    #fig.savefig('gas' + '{0:d}'.format(n + 1000) + '.jpg')
+
+
+def print_plot(n):
+    draw_3d()
 
 
 def make_x_step(t, x, y, v_x, v_y):
@@ -54,58 +67,69 @@ def make_y_step(t, x, y, v_x, v_y):
                                f[1][x][y][v_x][v_y]), 0)
 
 
-def count_collisions_with_walls(time):
+def count_x_collisions_with_walls(time):
     pre_time = (time - 1)%2
 
     for y in range(0, N):
         for v_x in range(number_of_velocities // 2 + 1, number_of_velocities):
+            a = abs((v_x - number_of_velocities // 2) * dt / dx)
             for v_y in range(number_of_velocities):
-                f[time][0][y][v_x][v_y] = f[pre_time][0][y][number_of_velocities - 1 - v_x][v_y]
-                f[pre_time][0][y][number_of_velocities - 1 - v_x][v_y] = 0
+                f[time][0][y][v_x][v_y] = (1-a)*f[pre_time][0][y][v_x][v_y] + \
+                                          a*f[pre_time][0][y][number_of_velocities - 1 - v_x][v_y];
 
     for y in range(0, N):
         for v_x in range(number_of_velocities // 2):
+            a = abs((v_x - number_of_velocities // 2) * dt / dx)
             for v_y in range(number_of_velocities):
-                f[time][N-1][y][v_x][v_y] = f[pre_time][N-1][y][number_of_velocities - 1 - v_x][v_y]
-                f[pre_time][0][y][number_of_velocities - 1 - v_x][v_y] = 0
+                f[time][N-1][y][v_x][v_y] = (1 - a) * f[pre_time][N-1][y][v_x][v_y] + \
+                                          a * f[pre_time][N-1][y][number_of_velocities - 1 - v_x][v_y];
+
+
+def count_y_collisions_with_walls(time):
+    pre_time = (time - 1)%2
 
     for x in range(0, N):
         for v_y in range(number_of_velocities // 2 + 1, number_of_velocities):
+            a = abs((v_y - number_of_velocities // 2) * dt / dy)
             for v_x in range(number_of_velocities):
-                f[time][x][0][v_x][v_y] = f[pre_time][x][0][v_x][number_of_velocities - 1 - v_y]
-                f[pre_time][x][0][v_x][number_of_velocities - 1 - v_y] = 0
+                f[time][x][0][v_x][v_y] = (1 - a) * f[pre_time][x][0][v_x][v_y] + \
+                                          a * f[pre_time][x][0][v_x][number_of_velocities - 1 - v_y];
 
     for x in range(0, N):
         for v_y in range(number_of_velocities // 2):
+            a = abs((v_y - number_of_velocities // 2) * dt / dy)
             for v_x in range(number_of_velocities):
-                f[time][x][N-1][v_x][v_y] = f[pre_time][x][N-1][v_x][number_of_velocities - 1 - v_y]
-                f[pre_time][x][N - 1][v_x][number_of_velocities - 1 - v_y] = 0
+                f[time][x][N-1][v_x][v_y] = (1 - a) * f[pre_time][x][N-1][v_x][v_y] + \
+                                          a * f[pre_time][x][N-1][v_x][number_of_velocities - 1 - v_y];
 
 
+fill_space(is_one_velocity=True)
 
-fill_space()
 
-for time in range(0, Per - 1, 2):
+def count_two_steps(n_step):
+    concentration = np.zeros(N * N ).reshape( N, N)
     for x in range(0, N - 1):  # Making step using only x
-        for y in range(0, N - 1):
+        for y in range(0, N):
             for v_x in range(number_of_velocities):
                 for v_y in range(number_of_velocities):
                     make_x_step(1, x, y, v_x, v_y)
                     concentration[x][y] += f[1][x][y][v_x][v_y]
             # concentration[x][y] = f[1][x][y][2][2] # We need this only to check, how does it work with one speed
-    count_collisions_with_walls(1)
+    count_x_collisions_with_walls(1)
 
-    draw_3d(concentration, time)
-    concentration = np.zeros_like(concentration)
+    concentration = np.zeros(N * N).reshape(N, N)
 
-    for x in range(0, N - 1):  # making step using only y
+    for x in range(0, N):  # making step using only y
         for y in range(0, N - 1):
             for v_x in range(number_of_velocities):
                 for v_y in range(number_of_velocities):
                     make_y_step(0, x, y, v_x, v_y)
                     concentration[x][y] += f[0][x][y][v_x][v_y]
             # concentration[x][y] = f[0][x][y][2][2] # We need this only to check, how does it work with one speed
-    count_collisions_with_walls(0)
+    count_y_collisions_with_walls(0)
 
-    draw_3d(concentration, time + 1)
-    concentration = np.zeros_like(concentration)
+    return draw_3d(concentration)
+
+
+_animation = FuncAnimation(fig, count_two_steps, interval=5, repeat=True, frames=Per - 1)
+plt.show()
