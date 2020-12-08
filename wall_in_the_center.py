e52a345b0a04d09b9ce59dbd7728ab2de8b48515
valerev <valerev.da@phystech.edu>
2020-12-08 00:43:44 +0000

    Added gas going into space

--- wall_in_the_center.py
+++ wall_in_the_center.py
@@ -16,16 +16,18 @@ prev_data = data.copy()
 
 concent = 100
 
+sigma = 1  
+
 wall_x_phy = 15
 wall_y_phy = 50
 
 #wall_x = (wall_x_phy * n_x) // 100
-wall_x = 3
+wall_x = 2
 wall_y = (wall_y_phy * n_y) // 100
 
 x_data = []
 y1_data = []
-y2_data = []
+#y2_data = []
 
 fig = plt.figure()
 axes = plt.axes(
@@ -76,7 +78,7 @@ def initial_f(i_x, i_y, i_vx, i_vy):
 
     if i_y < wall_y:
         return 0.004
-    return 0.001
+    return 0.00001
 
 def initialize():
     for i_vx in range(n_vx):
@@ -157,22 +159,22 @@ def save_to_file(filename, array):
         f.writelines(lines)
 
 
-def easy_x_diffusion():
-    S_pos_x = numpy.add.reduce(prev_data[0, :, n_vx//2+1:, :], (1, 2))
-    S_neg_x = numpy.add.reduce(prev_data[n_x - 1, :, :n_vx//2, :], (1, 2)) 
+def easy_x_diffusion(alpha):
+    S_pos_x = numpy.add.reduce(prev_data[0, :wall_y, n_vx//2+1:, :], (1, 2))
+    S_neg_x = numpy.add.reduce(prev_data[n_x - 1, :wall_y, :n_vx//2, :], (1, 2)) 
     S_wall_x = numpy.add.reduce(prev_data[wall_x - 1, wall_y, :n_vx//2, :], (0, 1))
  
     for i_vx in range(n_vx):
         for i_vy in range(n_vy):
             v = get_v(i_vx, i_vy)
             if v[0] < 0:
-                data[0, :, i_vx, i_vy] = S_pos_x / ((n_vx//2) * n_vy)
+                data[0, :wall_y, i_vx, i_vy] = alpha * S_pos_x / ((n_vx//2) * n_vy)
             elif v[0] > 0:
-                data[n_x - 1, :, i_vx, i_vy] = S_neg_x / ((n_vx//2) * n_vy)
-                data[wall_x - 1, wall_y, i_vx, i_vy] = S_wall_x / ((n_vx//2) * n_vy)
+                data[n_x - 1, :wall_y, i_vx, i_vy] = alpha * S_neg_x / ((n_vx//2) * n_vy)
+                data[wall_x - 1, wall_y, i_vx, i_vy] = alpha * S_wall_x / ((n_vx//2) * n_vy)
 
 
-def easy_y_diffusion():
+def easy_y_diffusion(alpha):
     S_pos_y = numpy.add.reduce(prev_data[:, 0, :, n_vy//2+1:], (1, 2)) 
     S_neg_y = numpy.add.reduce(prev_data[:, n_y - 1, :, :n_vy//2], (1, 2))
     S_out_y = numpy.add.reduce(prev_data[wall_x:, wall_y + 1, :, n_vy//2+1:], (1, 2))
@@ -182,34 +184,34 @@ def easy_y_diffusion():
         for i_vy in range(n_vy):
             v = get_v(i_vx, i_vy)
             if v[1] < 0:
-                data[:, 0, i_vx, i_vy] = S_pos_y / (n_vx * (n_vy//2))
-                data[wall_x:, wall_y + 1, i_vx, i_vy] = S_out_y / (n_vx * (n_vy//2))
+                data[:, 0, i_vx, i_vy] = alpha * S_pos_y / (n_vx * (n_vy//2))
+                data[wall_x:, wall_y + 1, i_vx, i_vy] = alpha * S_out_y / (n_vx * (n_vy//2))
             elif v[1] > 0:
-                data[:, n_y - 1, i_vx, i_vy] = S_neg_y / (n_vx * (n_vy//2))
-                data[wall_x:, wall_y - 1, i_vx, i_vy] = S_wal_y / (n_vx * (n_vy//2))
+                #data[:, n_y - 1, i_vx, i_vy] = alpha * S_neg_y / (n_vx * (n_vy//2))
+                data[wall_x:, wall_y - 1, i_vx, i_vy] = alpha * S_wal_y / (n_vx * (n_vy//2))
 
 
-def x_reflection():
-    for i_y in range(n_y):
-        for i_vx in range(n_vx):
-            for i_vy in range(n_vy):
-                v = get_v(i_vx, i_vy)
-                if v[0] < 0:
-                    data[0, i_y, i_vx, i_vy] = prev_data[0, i_y, n_vx - i_vx - 1, i_vy]
-                else:
-                    data[n_x - 1, i_y, i_vx, i_vy] = prev_data[n_x - 1, i_y, n_vx - i_vx - 1, i_vy]
-                    data[wall_x - 1, wall_y, i_vx, i_vy] = prev_data[wall_x - 1, wall_y, n_vx - i_vx - 1, i_vy]
+def x_reflection(beta):
+    for i_vx in range(n_vx):
+        for i_vy in range(n_vy):
+            v = get_v(i_vx, i_vy)
+            if v[0] < 0:
+                data[0, :, i_vx, i_vy] += beta * prev_data[0, :, n_vx - i_vx - 1, i_vy]
+            else:
+                data[n_x - 1, :, i_vx, i_vy] += beta * prev_data[n_x - 1, :, n_vx - i_vx - 1, i_vy]
+                data[wall_x - 1, wall_y, i_vx, i_vy] += beta * prev_data[wall_x - 1, wall_y, n_vx - i_vx - 1, i_vy]
 
 
-def y_reflection():
-    for i_x in range(n_x):
-        for i_vx in range(n_vx):
-            for i_vy in range(n_vy):
-                v = get_v(i_vx, i_vy)
-                if v[1] < 0:
-                    data[i_x, 0, i_vx, i_vy] = prev_data[i_x, 0, i_vx, n_vy - i_vy - 1]
-                else:
-                    data[i_x, n_y - 1, i_vx, i_vy] = prev_data[i_x, n_y - 1, i_vx, n_vy - i_vy - 1]
+def y_reflection(beta):
+    for i_vx in range(n_vx):
+        for i_vy in range(n_vy):
+            v = get_v(i_vx, i_vy)
+            if v[1] < 0:
+                data[:, 0, i_vx, i_vy] += beta * prev_data[:, 0, i_vx, n_vy - i_vy - 1]
+                data[wall_x:, wall_y + 1, i_vx, i_vy] += beta * prev_data[wall_x:, wall_y + 1, i_vx, n_vy - i_vy - 1]
+            else:
+                data[:, n_y - 1, i_vx, i_vy] += beta * prev_data[:, n_y - 1, i_vx, n_vy - i_vy - 1]
+                data[wall_x:, wall_y - 1, i_vx, i_vy] += beta * prev_data[wall_x:, wall_y - 1, i_vx, n_vy - i_vy - 1]
 
 
 def calc_epoch(i):
@@ -254,7 +256,7 @@ def calc_epoch(i):
     #            data[n_x - 1, :wall_y, i_vx, i_vy] = numpy.exp(-0.5*(v[0]**2 + v[1]**2))*S_neg_x/S_x
     #            data[wall_x - 1, wall_y, i_vx, i_vy] = numpy.exp(-0.5*(v[0]**2 + v[1]**2))*S_wall_x/S_x
      
-    easy_x_diffusion()
+    easy_x_diffusion(sigma)
 
     data, prev_data = prev_data, data
 
@@ -295,14 +297,14 @@ def calc_epoch(i):
     #        elif v[1] > 0:
     #            data[wall_x:, wall_y - 1, i_vx, i_vy] = numpy.exp(-0.5*(v[0]**2 + v[1]**2))*S_neg_y/S_y
    
-    easy_y_diffusion()
+    easy_y_diffusion(sigma)
 
     #print (numpy.add.reduce(data[:, 0, :, :3], (0, 1, 2)) / numpy.add.reduce(prev_data[:, 0, :, 4:], (0, 1, 2)))
     print ('\n')
 
     x_data.append(i)
     y1_data.append(numpy.add.reduce(data[:, :wall_y, :, :], (0, 1, 2, 3)))
-    y2_data.append(numpy.add.reduce(data[:, wall_y + 1:, :, :], (0, 1, 2, 3)))
+    #y2_data.append(numpy.add.reduce(data[:, wall_y + 1:, :, :], (0, 1, 2, 3)))
 
     data, prev_data = prev_data, data
 
@@ -313,7 +315,7 @@ def calc_epoch(i):
     print(f"{time}. Step {i}. Total count: {total_count}")
     #save_to_file(f"out_{i:03}.dat", concentration)
     #return total_count
-    #return draw(concentration)
+    return draw(concentration)
 
 
 def main():
@@ -335,17 +337,17 @@ main()
 for i in range(n_epoch):
     calc_epoch(i)
 
-file1 = open('data(in)_wall_3', 'w')
+file1 = open('into_space_2', 'w')
 for num in y1_data:
     file1.write(str(num) + '\n')
 file1.close()
 
-file2 = open('data(out)_wall_3', 'w')
-for num in y2_data:
-    file2.write(str(num) + '\n')
-file2.close()
+#file2 = open('data(out)_wall_30', 'w')
+#for num in y2_data:
+#    file2.write(str(num) + '\n')
+#file2.close()
 
 axes.plot(x_data, y1_data)
-axes.plot(x_data, y2_data)
+#axes.plot(x_data, y2_data)
 
 plt.show()
